d701914a03e171a71acf9269049c2c7a10323a54
pakyrs <56080974+pakyrs@users.noreply.github.com>
2020-12-21 11:20:04 +0000

    Update win10script_clients.ps1

--- win10script_clients.ps1
+++ win10script_clients.ps1
@@ -13,21 +13,21 @@ choco install firefox -y
 choco install notepadplusplus -y
 #choco install putty -y
 Write-Host "Installing Runtimes"
-choco install vcredist140 -y
-choco install adobeair -y
-choco install dotnetfx -y
-choco install silverlight -y
+#choco install vcredist140 -y
+#choco install adobeair -y
+#choco install dotnetfx -y
+#choco install silverlight -y
 Write-Host "Installing Commercial Apps"
 choco install adobereader -y
 #choco install shutup10 -y
-choco install googleearthpro -y
+#choco install googleearthpro -y
 choco install office365business -y
-choco install microsoft-teams.install -y
+#choco install microsoft-teams.install -y
 ##Creative Cloud is not silent, please sign in and also deploy with trial PS IN IL ACDC (might need reboot)
-choco install adobe-creative-cloud -y
+#choco install adobe-creative-cloud -y
 
 ##Enable NET3.5 for legacy products (Bentley)
-Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3"
+#Enable-WindowsOptionalFeature -Online -FeatureName "NetFx3"
 
 ##Windows Privacy Settings with O&O Shutup NEED REVIEWING THIS List
     Write-Host "Running O&O Shutup with Recommended Settings"
